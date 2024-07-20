aba1adb317e9730a4e73543e093a194e00143100
lukblan <luk.blanko@gmail.com>
2024-07-20 17:15:16 -0300

    Add artwork_shares routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :destroy, :index, :show, :update]
   resources :artworks, only: [:index, :show, :destroy, :create, :update]
+  resources :artwork_shares, only: [:create, :destroy]
 end
