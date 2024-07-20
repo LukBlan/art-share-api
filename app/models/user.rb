b9cf39ce5bcce520a4158211ff88b9393aabbe90
lukblan <luk.blanko@gmail.com>
2024-07-20 17:15:47 -0300

    Fix artwork destroy on artwork_share destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   validates :username, presence: true
 
-  has_many :artwork_shares, foreign_key: :viewer_id, class_name: "ArtworkShare"
+  has_many :artwork_shares, foreign_key: :viewer_id, class_name: "ArtworkShare", dependent: :destroy
   has_many :artworks, foreign_key: :artist_id, class_name: "Artwork", dependent: :destroy
-
 
   has_many :shares_artworks, through: :artwork_shares, source: :artwork
 end
